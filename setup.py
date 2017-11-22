135094f8ddd6cff50fb53e46dd35e460fad2a5aa
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-11-22 10:57:28 +0000

    Adds setup.py.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+try:
+    from pypandoc import convert
+    def read_markdown(file: str) -> str:
+        return convert(file, "rst")
+except ImportError:
+    def read_markdown(file: str) -> str:
+        return open(file, "r").read()
+
+setup(
+    name="capturewrap",
+    version="1.0.0",
+    packages=find_packages(exclude=["tests"]),
+    url="https://github.com/wtsi-hgi/python-capturewrap",
+    license="MIT",
+    description="Wraps callables to capture stdout, stderr, exceptions and the return",
+    long_description=read_markdown("README.md")
+)
